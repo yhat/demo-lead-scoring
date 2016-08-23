575dc54dd7fd08db3c9a31d12de5ebe9e30724c0
Greg Lamp <lamp.greg@gmail.com>
2016-08-23 11:50:27 -0400

    deployBatch. yaml file.

--- lead-scorer.R
+++ lead-scorer.R
@@ -1,45 +1,64 @@
 
 
-df <- read.csv("data/leads-and-customers.csv")
-head(df)
+main = function() {
+# df <- read.csv("data/leads-and-customers.csv")
+  df <- read.csv("https://github.com/yhat/demo-lead-scoring/blob/master/data/leads-and-customers.csv?raw=true")
+  head(df)
 
-df$is_manager <- grepl("manager|director|supervisor", df$job_title, ignore.case = TRUE)
+  df$is_manager <- grepl("manager|director|supervisor", df$job_title, ignore.case = TRUE)
 
-head(df)
+  head(df)
 
-company_size_levels <- c("1-10", "11-50", "51-100", "101-250", "251-1000", "1000-10000", "10001+")
-df$company_size <- factor(df$company_size, levels=company_size_levels)
+  company_size_levels <- c("1-10", "11-50", "51-100", "101-250", "251-1000", "1000-10000", "10001+")
+  df$company_size <- factor(df$company_size, levels=company_size_levels)
 
-for (f in c('is_manager', 'days_since_signup', 'visited_pricing', 'registered_for_webinar', 'attended_webinar', 'completed_form')) {
-  print(f)
-  print(table(df[,f], df$converted))
-  print(paste0(rep("*", 80), collapse=""))
-}
+  for (f in c('is_manager', 'days_since_signup', 'visited_pricing', 'registered_for_webinar', 'attended_webinar', 'completed_form')) {
+    print(f)
+    print(table(df[,f], df$converted))
+    print(paste0(rep("*", 80), collapse=""))
+  }
 
-features <- c(
-  "is_manager",
-  "days_since_signup",
-  "completed_form",
-  "visited_pricing",
-  "registered_for_webinar",
-  "attended_webinar",
-  "acquisition_channel",
-  "company_size",
-  "industry",
-  "converted"
-)
+  features <- c(
+    "is_manager",
+    "days_since_signup",
+    "completed_form",
+    "visited_pricing",
+    "registered_for_webinar",
+    "attended_webinar",
+    "acquisition_channel",
+    "company_size",
+    "industry",
+    "converted"
+  )
 
-library(randomForest)
+  library(randomForest)
 
 
-logit <- glm(converted ~ ., data=df[,features])
-print(summary(logit))
+  logit <- glm(converted ~ ., data=df[,features])
+  print(summary(logit))
 # rf <- randomForest(converted ~ ., data=df[,features])
 
 
-probs <- predict(logit, newdata=df, type="response")
-grades <- cut(probs, 5, labels=c("F","D","C","B","A"), ordered_result=TRUE)
+  probs <- predict(logit, newdata=df, type="response")
+  grades <- cut(probs, 5, labels=c("F","D","C","B","A"), ordered_result=TRUE)
 
-lead_quality <- table(grades)
-o <- order(names(lead_quality))
-print(lead_quality[o])
+  lead_quality <- table(grades)
+  o <- order(names(lead_quality))
+  print(lead_quality[o])
+}
+
+# library(yhatr)
+source("~/workspace/github.com/yhat/yhatr/R/yhatR.R")
+yhat.library("randomForest")
+
+model.predict <- function() {
+  main()
+}
+
+yhat.config <- c(
+  username="",
+  apikey="",
+  env=""
+)
+
+yhat.batchDeploy("lead_scorer")
